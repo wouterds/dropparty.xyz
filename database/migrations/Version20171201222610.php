586f2d46b3db44c92b54e5aded20aad567cbc193
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2017-12-02 22:43:08 +0100

    :card_file_box: Dropbox token migration

---
+++ database/migrations/Version20171201222610.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace DropParty\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20171201222610 extends AbstractMigration
+{
+    private const TABLE_NAME = 'dropbox_token';
+
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $table = $schema->createTable(self::TABLE_NAME);
+        $table->addColumn('user_id', 'uuid');
+        $table->addColumn('access_token', 'text');
+        $table->addColumn('created_at', 'datetime')->setDefault('CURRENT_TIMESTAMP');
+        $table->addColumn('updated_at', 'datetime')->setNotnull(false);
+        $table->setPrimaryKey(['user_id']);
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $schema->dropTable(self::TABLE_NAME);
+    }
+}
