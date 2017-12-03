9e36bf4a325cb2553283e444e896dbb2e056b1d4
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2017-12-03 22:40:25 +0100

    :fire: Removed Api/Files/DownloadHandler

--- src/Application/Http/routes-api.php
+++ src/Application/Http/routes-api.php
@@ -1,12 +1,10 @@
 <?php
 
 use DropParty\Application\Http\Handlers\Api\Files\AddHandler as ApiFilesAddHandler;
-use DropParty\Application\Http\Handlers\Api\Files\DownloadHandler as ApiFilesDownloadHandler;
 use DropParty\Application\Http\Handlers\Api\Files\ListHandler as ApiFilesListHandler;
 use DropParty\Application\Http\Handlers\Api\NotFoundHandler as ApiNotFoundHandler;
 use DropParty\Application\Http\Handlers\Api\Users\AuthenticateHandler as ApiUsersAuthenticateHandler;
 use DropParty\Application\Http\Handlers\Api\Users\RegisterHandler as ApiUsersRegisterHandler;
-use DropParty\Application\Http\Middlewares\FilesAccessLogsMiddleware;
 
 $app->get('/', ApiNotFoundHandler::class);
 
@@ -17,6 +15,5 @@ $app->group('/users', function () use ($app) {
 
 $app->group('/files', function () use ($app) {
     $app->get('.list', ApiFilesListHandler::class)->setName('api.files.list');
-    $app->get('.download', ApiFilesDownloadHandler::class)->setName('api.files.download')->add(FilesAccessLogsMiddleware::class);
     $app->post('.add', ApiFilesAddHandler::class)->setName('api.files.add');
 });
