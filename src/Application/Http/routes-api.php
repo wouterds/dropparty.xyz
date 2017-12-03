135409889bf04d8dcc7ddcd5bffef61fa300c9eb
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2017-12-03 22:44:24 +0100

    :fire: Removed Api/Files/ListHandler

--- src/Application/Http/routes-api.php
+++ src/Application/Http/routes-api.php
@@ -1,7 +1,6 @@
 <?php
 
 use DropParty\Application\Http\Handlers\Api\Files\AddHandler as ApiFilesAddHandler;
-use DropParty\Application\Http\Handlers\Api\Files\ListHandler as ApiFilesListHandler;
 use DropParty\Application\Http\Handlers\Api\NotFoundHandler as ApiNotFoundHandler;
 use DropParty\Application\Http\Handlers\Api\Users\AuthenticateHandler as ApiUsersAuthenticateHandler;
 use DropParty\Application\Http\Handlers\Api\Users\RegisterHandler as ApiUsersRegisterHandler;
@@ -14,6 +13,5 @@ $app->group('/users', function () use ($app) {
 });
 
 $app->group('/files', function () use ($app) {
-    $app->get('.list', ApiFilesListHandler::class)->setName('api.files.list');
     $app->post('.add', ApiFilesAddHandler::class)->setName('api.files.add');
 });
