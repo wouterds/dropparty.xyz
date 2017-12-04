a05567e2e4d24de06cb1c217176d296f54af1570
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2017-12-04 12:35:53 +0100

    :fire: Removed API RegisterHandler

--- src/Application/Http/routes-api.php
+++ src/Application/Http/routes-api.php
@@ -3,12 +3,10 @@
 use DropParty\Application\Http\Handlers\Api\Files\AddHandler as ApiFilesAddHandler;
 use DropParty\Application\Http\Handlers\Api\NotFoundHandler as ApiNotFoundHandler;
 use DropParty\Application\Http\Handlers\Api\Users\AuthenticateHandler as ApiUsersAuthenticateHandler;
-use DropParty\Application\Http\Handlers\Api\Users\RegisterHandler as ApiUsersRegisterHandler;
 
 $app->get('/', ApiNotFoundHandler::class);
 
 $app->group('/users', function () use ($app) {
-    $app->post('.register', ApiUsersRegisterHandler::class)->setName('api.users.register');
     $app->post('.authenticate', ApiUsersAuthenticateHandler::class)->setName('api.users.authenticate');
 });
 
