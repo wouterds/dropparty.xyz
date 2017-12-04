f9b5406e5933c61b4966e0ecfb92e99b7b30ad92
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2017-12-04 22:51:04 +0100

    :sparkles: Show message if Dropbox account linked

--- src/Application/Http/Handlers/AccountHandler.php
+++ src/Application/Http/Handlers/AccountHandler.php
@@ -2,12 +2,32 @@
 
 namespace DropParty\Application\Http\Handlers;
 
+use DropParty\Domain\Dropbox\TokenRepository;
+use DropParty\Domain\Users\UserId;
+use DropParty\Infrastructure\ApplicationMonitor\ApplicationMonitor;
 use DropParty\Infrastructure\Http\Handlers\AbstractViewHandler;
+use DropParty\Infrastructure\View\Twig;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
 class AccountHandler extends AbstractViewHandler
 {
+    /**
+     * @var TokenRepository
+     */
+    private $tokenRepository;
+
+    /**
+     * @param Twig $twig
+     * @param ApplicationMonitor $applicationMonitor
+     * @param TokenRepository $tokenRepository
+     */
+    public function __construct(Twig $twig, ApplicationMonitor $applicationMonitor, TokenRepository $tokenRepository)
+    {
+        parent::__construct($twig, $applicationMonitor);
+        $this->tokenRepository = $tokenRepository;
+    }
+
     /**
      * @return string
      */
@@ -23,6 +43,11 @@ class AccountHandler extends AbstractViewHandler
      */
     public function __invoke(Request $request, Response $response): Response
     {
-        return $this->render($request, $response);
+        $data = [];
+
+        $userId = new UserId($request->getCookieParam('uid'));
+        $data['dropboxLinked'] = $this->tokenRepository->hasTokenForUserId($userId);
+
+        return $this->render($request, $response, $data);
     }
 }
